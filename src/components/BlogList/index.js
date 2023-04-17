d23723455237e41eb3388ff809752088b12f7bdc
Rajashekhar V <rajashekharv.rymec@gmail.com>
2023-04-17 09:25:53 +0000

    debugged fetch-routing-practice-app

---
+++ src/components/BlogList/index.js
@@ -0,0 +1,59 @@
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+// bugs identified are
+// 1. Async function missed before arrow function of getsBlogsData
+// 2. not converted obtained jso string data into json object format after response also await key word need to use before json().
+// 3. formattedData is not updated in the state
+// 4. made infinite loop by calling the getBlogsData() inside render() method
+// 5. not passed unique key id as a prop on to the react children elements to identify uniquely.
+// 6. fetch keyword wrongly spelled.
+// 7. data-testid value is misspelled as loder.
+class BlogList extends Component {
+  state = {isLoading: true, blogsData: []}
+
+  componentDidMount() {
+    this.getBlogsData()
+  }
+
+  getBlogsData = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const data = await response.json()
+    const formattedData = data.map(eachItem => ({
+      id: eachItem.id,
+      title: eachItem.title,
+      imageUrl: eachItem.image_url,
+      avatarUrl: eachItem.avatar_url,
+      author: eachItem.author,
+      topic: eachItem.topic,
+    }))
+    this.setState({isLoading: false, blogsData: formattedData})
+  }
+
+  render() {
+    const {blogsData, isLoading} = this.state
+
+    return (
+      <div className="blogs-list-container">
+        {isLoading ? (
+          <div data-testid="loader">
+            <Loader type="TailSpin" color="#00bfff" height={50} width={50} />
+          </div>
+        ) : (
+          <ul className="blogs-list">
+            {blogsData.map(eachBlogItem => (
+              <BlogItem key={eachBlogItem.id} blogItemDetails={eachBlogItem} />
+            ))}
+          </ul>
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogList
